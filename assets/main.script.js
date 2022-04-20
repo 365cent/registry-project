4d053dd680657e5bb66f48ba7d60b824fa09df6a
llor <cwheric1999@gmail.com>
2022-04-20 13:00:22 -0400

    add error msg responed

--- assets/main.script.js
+++ assets/main.script.js
@@ -37,3 +37,32 @@ window.addEventListener('load', function () {
 		},
 	});
 });
+
+let usernamevalid = false;
+
+window.addEventListener("DOMContentLoaded", () => {
+	const username = document.getElementById("username");
+	username.addEventListener("blur", () => { //add event listener for blur
+	var errMsg = document.getElementById("error");
+	if(errMsg){ //if there is an error message already, remove it
+		errMsg.remove();
+	}
+	fetch('api.php?username=' + username.value) //fetch the data from the api
+		//.then(response => response.json())
+		.then(response => response.text())
+		.then(response => {
+			if(response == 'false' || response == 'error') { // if the response is false or error, show the error message
+				var errMsg = document.createElement('span');
+				errMsg.classList.add('error');
+				errMsg.setAttribute('id', 'error');
+				errMsg.innerText = 'username already taken';
+				emailInput.parentNode.insertBefore(errMsg, username.nextSibling);
+				emailvalid = false;
+			}
+			else {
+				emailvalid = true;
+			}
+		}
+	);
+});
+})
