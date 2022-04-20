f877c4a75a8c07e9000b4287c25a37424f72e00c
llor <cwheric1999@gmail.com>
2022-04-20 16:19:20 -0400

    seg error check

--- assets/main.script.js
+++ assets/main.script.js
@@ -11,9 +11,17 @@ function validate(event) {
 		hcaptcha.execute();
 	}
 }
+function usernameIsValid(username) {
+	return /^[0-9a-zA-Z_.-]+$/.test(username);
+}
+//https://www.jianshu.com/p/c35d78385feb
+function passwordIsValid(password) {
+	return /^(?![0-9]+$)(?![a-z]+$)(?![A-Z]+$)(?!([^(0-9a-zA-Z)])+$).{6,20}$/.test(password);
+}
 window.addEventListener('load', function () {
 	let menuOpen = false;
 	let usernameValid = false;
+	let passwordValid = false;
 	document.querySelector('nav details summary').addEventListener('click', function () {
 		menuOpen = !menuOpen;
 		if (menuOpen) {
@@ -38,23 +46,37 @@ window.addEventListener('load', function () {
 		let submit = document.querySelector('form button');
 		let err = document.getElementsByClassName('err')[0];
 		submit.onclick = validate;
+
 		if (location.pathname.includes('signup')) {
 			const username = document.getElementsByName("username")[0];
 			username.addEventListener("blur", () => { //add event listener for blur
 				err.innerText = '';
-				fetch('includes/api.php?username=' + username.value) //fetch the data from the api
-					//.then(response => response.json())
-					.then(response => response.text())
-					.then(response => {
-						if (response == 'true') { // if the response is false or error, show the error message
-							err.innerText = 'Username already taken';
-							usernameValid = false;
+				if(usernameIsValid(username.value)){
+					fetch('includes/api.php?username=' + username.value) //fetch the data from the api
+						//.then(response => response.json())
+						.then(response => response.text())
+						.then(response => {
+							if (response == 'true') { // if the response is false or error, show the error message
+								err.innerText = 'Username already taken';
+								usernameValid = false;
+							}
+							else {
+								usernameValid = true;
+							}
 						}
-						else {
-							usernameValid = true;
-						}
-					}
-					);
+						);	
+				}else{
+					err.innerText = 'Username is nort valid';
+					usernameValid = false;
+				}
+			
+				if(passwordIsValid(document.getElementsByName("password")[0].value)){
+					err.innerHTML = '';
+				}else{
+					err.innerText = 'password is not valid';
+					passwordValid = false;
+				}
+				
 			});
 		}
 	} 
