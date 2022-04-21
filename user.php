ce48350f5a020c7865b871f6a27223489b480423
junech <junechting@gmail.com>
2022-04-21 01:56:51 -0400

    Update user profile and list page

--- user.php
+++ user.php
@@ -13,53 +13,41 @@ function update($new,$place){
         $pdo = connectDB();
         $sql = "UPDATE users set ?= ? WHERE id = ?";
         $stmt = $pdo ->prepare($sql)->execute([$place,$new,$id]);  
+        alert("update successful!!");
     }catch(PDOException $e){
         echo alert($e->getmessage());
     }
-	
 }
 
-function confirm($var,$confirm){
-	echo '<script> if(confirm("'.$var.'")){'$confirm = true' }else{'$confirm = false' }</script>';
-}
 function alert($var){
 	echo '<script>alert("'.$var.'")</script>';
 }
-$_POST['username'] =1;
+
 if(isset($_POST['username'])){
-    confirm("Are your sure change your user name?",$confirm);
-	// if(confirm("Are your sure change your user name?")){
-	// 	//update the datebase of new user name 
-	// 	if (preg_match($usernamevalid, $Cname)) {
-	// 		//update($Cname,username);
-    //         var_dump($confirm);
-	// 	}else{
-    //         var_dump($confirm);
-	// 		alert('new user name is not valid');
-	// 	}
-	// }
+    if (preg_match($usernamevalid, $Cname)) {
+        update($Cname,username);
+    }else{
+        alert('new user name is not valid');
+    }
+}
+if(isset($_POST['email'])){
+	
+    //update the datebase of new user name 
+    if (preg_match($emailvalid, $Cemail)) {
+        update($Cemail,email);
+    }else{
+        alert('new email name is not valid');
+    }
 	
 }
-// if(isset($_POST['email'])){
-// 	if(confirm("Are your sure change your email?")){
-// 		//update the datebase of new user name 
-// 		if (preg_match($emailvalid, $Cemail)) {
-// 			update($Cemail,email);
-// 		}else{
-// 			alert('new email name is not valid');
-// 		}
-// 	}
-// }
-// if(isset($_POST['password'])){
-// 	if(confirm("Are your sure change your password?")){
-// 		//update the datebase of new user name 
-// 		if (preg_match($passwordvalid, $Cpassword)) {
-// 			update($Cpassword,password);
-// 		}else{
-// 			alert('new user name is not valid');
-// 		}
-// 	}
-// }
+if(isset($_POST['password'])){
+	//update the datebase of new user name 
+    if (preg_match($passwordvalid, $Cpassword)) {
+        update($Cpassword,password);
+    }else{
+        alert('new password is not valid');
+    }
+}
 ?>
 
 
